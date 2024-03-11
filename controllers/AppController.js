d2c320a5ddd3c7e4430acaaa3fc9613649d10f6a
Rashnotech <rashnotech@gmail.com>
2024-03-11 12:07:41 +0000

    First Api

---
+++ controllers/AppController.js
@@ -0,0 +1,29 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+
+class AppController {
+
+  static getStatus (req, res) {
+    const response = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive()}
+    };
+    res.status(200);
+    res.json(response);
+  }
+
+  static getStats (req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([users, files]) => {
+        const response = {
+          users,
+          files,
+	};
+	res.status(200);
+	res.json(response);
+      });
+  }
+}
+
+export default AppController;
