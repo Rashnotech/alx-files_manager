f94781ae50b9b95d7fce292843ee1bf0141fdfd1
Rashnotech <rashnotech@gmail.com>
2024-03-11 11:42:13 +0000

    Mongo db connection

--- utils/redis.js
+++ utils/redis.js
@@ -1,27 +1,26 @@
-import { createClient } from 'redis'
-import { promisify } from 'util'
-
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor () {
     this.client = createClient();
     this.client.on('error', (err) => {
-      console.log(`Redis client cannot connect to server ${err}`)
-    })
+      console.log(`Redis client cannot connect to server ${err}`);
+    });
   }
 
   isAlive () {
-      return this.client.connected;
+    return this.client.connected;
   }
-  
+
   async get (key) {
-    const getAsync = promisify(this.client.get).bind(this.client)
-    return getAsync(key)
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
   }
 
   async set (key, value, duration) {
-    const setAsync = promisify(this.client.set).bind(this.client)
-    await setAsync(key, value, 'EX', duration)
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
   }
 
   async del (key) {
